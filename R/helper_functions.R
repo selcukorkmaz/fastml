33ffa3393ec18405a30f09df2c37fa621a0f8f8c
Dinçer GÖKSÜLÜK <dincer.goksuluk@gmail.com>
2024-12-20 12:41:16 +0300

    helper functions added.
    availableMethods() function is added to define supported algorithms in fastml.

---
+++ R/helper_functions.R
@@ -0,0 +1,43 @@
+availableMethods <- function(type = c("classification", "regression"), ...){
+  type <- match.arg(tye)
+
+  algorithms <- if (type == "classification"){
+    c(
+      "logistic_regression",
+      "penalized_logistic_regression",
+      "decision_tree",
+      "c5.0",
+      "random_forest",
+      "ranger",
+      "xgboost",
+      "lightgbm",
+      "svm_linear",
+      "svm_radial",
+      "knn",
+      "naive_bayes",
+      "neural_network",
+      "lda",
+      "qda",
+      "bagging"
+    )
+  } else {
+    c(
+      "linear_regression",
+      "ridge_regression",
+      "lasso_regression",
+      "elastic_net",
+      "decision_tree",
+      "random_forest",
+      "xgboost",
+      "lightgbm",
+      "svm_linear",
+      "svm_radial",
+      "knn",
+      "neural_network",
+      "pls",
+      "bayes_glm"
+    )
+  }
+
+  return(algorithms)
+}
